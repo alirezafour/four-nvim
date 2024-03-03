e3d3d7a4c1a803f3226098df6310e865d879efc5
alirezafour <alirezafour@gmail.com>
2024-03-04 02:16:56 +0300

    remove unused mapping of cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -97,8 +97,6 @@ return {
       formatting = formatting_style,
 
       mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
