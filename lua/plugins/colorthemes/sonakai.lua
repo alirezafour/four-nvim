a0c10439778ba9d9cad4f88eff9ba688e66cd076
alirezafour <alirezafour@gmail.com>
2024-11-16 22:01:01 +0100

    change theme config

--- lua/plugins/colorthemes/sonakai.lua
+++ lua/plugins/colorthemes/sonakai.lua
@@ -4,8 +4,9 @@ return {
   priority = 1000,
   config = function()
     vim.o.background = "dark"
-    vim.g.sonokai_style = "espresso"
+    vim.g.sonokai_style = "maia"
     vim.g.sonokai_dim_inactive_windows = true
+    vim.g.sonokai_diagnostic_virtual_text = 'highlighted'
     vim.g.sonokai_colors_override = {
       bg0 = {"#121212", "235"},
       bg_dim = {"#242120", "236"},
